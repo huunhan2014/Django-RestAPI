aef1f8942191a90b26671ec1f17ee922abb89380
nhuunhan <nhan.nguyen@worldmosquito.org>
2020-07-27 10:14:34 +0700

    Update urls

--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rest.views import PostListCreateAPIView, PostDetailUpdateAPIView, UserViewSet, GroupViewSet, TypeViewSet, ProductViewSet, FieldPlanningRunViewSet
+from rest import views as rest_views
 from . import views
 from school import views as school_views
 
 router = routers.DefaultRouter()
 # router = routers.SimpleRouter()
 
-router.register(r'posts', PostListCreateAPIView)
-router.register(r'posts', PostDetailUpdateAPIView)
+router.register(r'posts', rest_views.PostListCreateAPIView)
+router.register(r'posts', rest_views.PostDetailUpdateAPIView)
 
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'types', TypeViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'field-plannning-runs', FieldPlanningRunViewSet)
+# Rest App
+router.register(r'users', rest_views.UserViewSet)
+router.register(r'groups', rest_views.GroupViewSet)
+router.register(r'types', rest_views.TypeViewSet)
+router.register(r'products', rest_views.ProductViewSet)
+router.register(r'field-plannning-runs', rest_views.FieldPlanningRunViewSet)
+
+# School App
 router.register(r'school-api', school_views.SchoolViewSet)
 router.register(r'class-api', school_views.ClassViewSet)
 router.register(r'student-api', school_views.StudentViewSet)
